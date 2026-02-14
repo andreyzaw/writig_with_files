fc3d82f0c251f2e7c658bb9b356438262852e605
zaw <zaw68@mail.ru>
2026-02-14 11:47:50 +1000

    Initial commit

---
+++ list_of_recipes.py
@@ -0,0 +1,43 @@
+def read_cook_book(file_name):
+# Creating cook book with a file
+    cook_book ={}
+    with open(file_name, encoding="utf-8") as f:
+# Reading cook and count ingradient
+        for line_file in f:
+            cook = line_file.strip()
+            count_ingr_str = f.readline().strip()
+            count_ingr = int(count_ingr_str)
+            recipe = []
+# Forming a list of ingredients
+            for i in range(count_ingr):
+                ingradiet = {}
+                str_ingradient = f.readline().strip().split(" | ")
+                ingradiet["ingredient_name"] = str_ingradient[0]
+                ingradiet["quantity"] = str_ingradient[1]
+                ingradiet["measure"] = str_ingradient[2]
+                recipe.append(ingradiet)
+            cook_book.setdefault(cook, recipe)
+            null_str = f.readline()
+
+    return cook_book
+
+def get_shop_list_by_dishes(dishes, person_count):
+# Forming a list of ingredients for dishes
+    cook_book = read_cook_book("recipes.txt")
+    dict_ingradients = {}
+    for dishe in dishes:
+#        print(dishe)
+        for ingradient in cook_book[dishe]:
+            count_ingradient = {}
+            count_ingradient["measure"] = ingradient["measure"]
+# Calculating count ingredients
+            if ingradient["ingredient_name"] not in dict_ingradients.keys():
+                count_ingradient["quantity"] = int(ingradient["quantity"]) * person_count
+            else:
+                count_ingradient["quantity"] += int(ingradient["quantity"]) * person_count
+            dict_ingradients[ingradient["ingredient_name"]] = count_ingradient
+    return dict_ingradients
+
+print(get_shop_list_by_dishes(['Запеченный картофель', 'Омлет'], 2))
+
+
---
+++ sort_files.py
@@ -0,0 +1,32 @@
+def count_line_fail(file_name):
+# Counting the number of lines in a file
+    with open(file_name, encoding="utf-8") as f:
+        line_file = f.readlines()
+        return len(line_file)
+
+def sort_files(list_file_names):
+    dict_files = {}
+# Creating dict {fail_name: count_line}
+    for fail_name in list_file_names:
+        dict_files[fail_name] =  count_line_fail(fail_name)
+# Sorting dict
+    sorted_dict ={}
+    for key in sorted(dict_files, key=dict_files.get):
+        sorted_dict[key] = dict_files[key]
+    return sorted_dict
+
+def write_in_file(file_name, sorted_file):
+    for f_n, c_l in sorted_file.items():
+        with open(file_name, 'a', encoding="utf-8") as f_w:
+# Recording of service information
+            f_w.write(f' {f_n}\n')
+            f_w.write(f'{c_l}\n')
+# Reading file and writing in result file
+            with open(f_n, encoding="utf-8") as f_r:
+                lines_file = f_r.readlines()
+            f_w.writelines(lines_file)
+            f_w.write(f'\n')
+    return
+
+dict_sort_files = sort_files(["sorted/1.txt", "sorted/2.txt", "sorted/3.txt"])
+write_in_file("sorted/result.txt", dict_sort_files)
